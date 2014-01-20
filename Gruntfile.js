3169f7055dca740f6d70db4d99159af95427a4e0
dmmn <ddotmnn+git@gmail.com>
2014-01-20 19:23:04 +0000

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function (grunt) {
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		watch: {
+			livereload: {
+				options: {
+					livereload: '<%= connect.livereload.options.livereload %>'
+				},
+				files: [
+					'**/*'
+				]
+			}
+		},
+		connect: {
+			livereload: {
+				options: {
+					port: 9000,
+					livereload: 35729,
+					open: 'http://localhost:9000',
+					hostname: '0.0.0.0',
+					base: ['experiments']
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('serve', [
+		'connect',
+		'watch'
+	]);
+};
