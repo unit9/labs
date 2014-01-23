53fc359c663bf07a588c315e0cdbba54763508ab
Silvio Paganini <s2paganini@gmail.com>
2014-01-23 22:11:36 +0000

    add experiments

---
+++ experiments/tent/js/plugins.js
@@ -0,0 +1,22 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var noop = function noop() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = window.console || {};
+
+    while (length--) {
+        // Only stub undefined methods.
+        console[methods[length]] = console[methods[length]] || noop;
+    }
+}());
+
+// ANALYTICS
+
+// Flash Trace (can't live without it)
+(function(){(window || document).trace = function(){console.log(arguments);};})();
